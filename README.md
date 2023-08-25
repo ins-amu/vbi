de15493ce81b06f1591014c5b405e8dd8bc63a16
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2023-08-25 13:02:42 +0200

    working on feature extraction

---
+++ README.md
@@ -0,0 +1,44 @@
+# Virtual Brain Inference (VBI)
+
+## installation
+
+### Requirements
+
+- Python3
+- Matplotlib
+- Scipy
+- numpy
+- C++ >= 11
+
+```sh
+conda env create --file environment.yml --name vbi
+conda activate vbi
+
+# gpu support
+# conda install -c conda-forge cupy cudatoolkit=11.3
+# conda install -c conda-forge pytorch-gpu
+
+# If you need to use models implemented in C++ :
+cd vbi/CPPModels
+make  
+# you need to install swig if you get an error and probably write the version of 
+# python you are using at makefile
+PYTHON_VERSION = 3.8 # or whatever version you have
+```
+
+you need to have install swig on you machine to compile C++ codes.
+
+```sh
+sudo apt-get install clang
+sudo apt-get install swig
+sudo apt-get install python3-dev # or [python3.9-dev] depends the default version of python on your machine.
+# unless you get an error which says: fatal error,  Python.h not found.
+```
+
+- documentation:
+
+```sh
+cd docs 
+doxygen Doxyfile
+make latexpdf      # for pdf file
+```
---
+++ setup.py
@@ -0,0 +1,30 @@
+# from sbi_nmms.__init__ import __version__ as v
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name='vbi',
+    version="0.1",
+    author="Abolfazl ziaeemehr, Meysam Hashemi",
+    author_email="a.ziaeemehr@gmail.com, meysam.hashemi@gmail.com",
+    description="Virtual brain inference",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ziaeemehr/vbi_paper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+    # python_requires='>=3.8',
+    # packages=['vbi'],
+    # package_dir={'vbi': 'vbi'},
+    # package_data={'vbi': ['CPPModels/*.so']},
+    # install_requires=requirements,
+    # include_package_data=True,
+)
