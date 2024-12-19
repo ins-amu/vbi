1d56ba0e3bd3f386ccba6c5cc38bea1e1e87dbf4
Abolfazl Ziaeemehr <abolfazl.ziaee-mehr@univ-amu.fr>
2024-12-19 17:59:53 +0100

    mpr_modified_bold as main mpr mode in cupy.

--- vbi/models/cupy/mpr.py
+++ vbi/models/cupy/mpr.py
@@ -1,3 +1,4 @@
+import os
 import tqdm
 import logging
 import numpy as np
@@ -9,8 +10,9 @@ try:
 except ImportError:
     logging.warning("Cupy is not installed. Using Numpy instead.")
 
+
 class MPR_sde:
-    '''
+    """
     Montbrio-Pazo-Roxin model Cupy and Numpy implementation.
 
     Parameters
@@ -24,7 +26,7 @@ class MPR_sde:
         - eta: model parameter
         - tau: model parameter
         - delta: model parameter
-        - decimate: sampling step from fmri
+        - tr: repetition time of fMRI
         - noise_amp: amplitude of noise
         - same_noise_per_sim: same noise for all simulations
         - iapp: external input
@@ -33,7 +35,7 @@ class MPR_sde:
         - t_end: end time
         - num_nodes: number of nodes
         - weights: weighted connection matrix
-        - record_step: sampling step from r and v
+        - rv_decimate: sampling step from r and v
         - output: output directory
         - RECORD_TS: store r and v time series
         - num_sim: number of simulations
@@ -45,9 +47,9 @@ class MPR_sde:
         - same_initial_state: same initial state for all simulations
         #!TODO: add more details about the parameters
         #TODO: add references
-    '''
+    """
 
-    def __init__(self, par:dict={}) -> None:
+    def __init__(self, par: dict = {}) -> None:
 
         self._par = self.get_default_parameters()
         self.valid_parameters = list(self._par.keys())
@@ -62,6 +64,8 @@ class MPR_sde:
         self.xp = get_module(self.engine)
         if self.seed is not None:
             self.xp.random.seed(self.seed)
+            
+        os.makedirs(self.output, exist_ok=True)
 
     def __call__(self):
         print("Montbrió, Pazó, Roxin model.")
@@ -78,60 +82,58 @@ class MPR_sde:
 
     def set_initial_state(self):
         self.initial_state = set_initial_state(
-            self.nn, self.num_sim, self.engine, self.seed,
-            self.same_initial_state, self.dtype)
+            self.nn,
+            self.num_sim,
+            self.engine,
+            self.seed,
+            self.same_initial_state,
+            self.dtype,
+        )
 
     def get_balloon_parameters(self):
-        '''get balloon model parameters.'''
-        alpha = 0.32
-        E0 = 0.4
-        eps = 0.5
-
+        """get balloon model parameters."""
         par = {
-            "eps": eps,
-            "E0": 0.4,
-            "V0": 4.0,
-            "alpha": alpha,
-            "inv_alpha": 1.0 / alpha,
-            "K1": 7.0 * E0,
-            "K2": 2 * E0,
-            "K3": 1 - eps,
-            "taus": 1.54,
-            "tauo": 0.98,
-            "tauf": 1.44,
+            "kappa": 0.65,
+            "gamma": 0.41,
+            "tau": 0.98,
+            "alpha": 0.32,
+            "epsilon": 0.34,
+            "Eo": 0.4,
+            "TE": 0.04,
+            "vo": 0.08,
+            "r0": 25.0,
+            "theta0": 40.3,
+            "t_min": 0.0,
+            "rtol": 1e-5,
+            "atol": 1e-8,
         }
-        par['inv_tauo'] = 1.0 / par['tauo']
-        par['inv_taus'] = 1.0 / par['taus']
-        par['inv_tauf'] = 1.0 / par['tauf']
         return par
 
     def get_default_parameters(self):
 
         par = {
-            "G": 0.72,                   # global coupling strength
-            "dt": 0.01,                  # dt for mpr model [ms]
-            "dt_bold": 0.001,            # dt for Balloon model [s]
-            "J": 14.5,                   # model parameter
+            "G": 0.72,  # global coupling strength
+            "dt": 0.01,  # dt for mpr model [ms]
+            "dt_bold": 0.001,  # dt for Balloon model [s]
+            "J": 14.5,  # model parameter
             "eta": -4.6,
-            "tau": 1.0,                  # model parameter
-            "delta": 0.7,                # model parameter
-
-            "decimate": 500,             # sampling step from fmri
-            "noise_amp": 0.037,          # amplitude of noise
-
+            "tau": 1.0,  # model parameter
+            "delta": 0.7,  # model parameter
+            "tr": 500.0,  # repetition time [ms]
+            "noise_amp": 0.037,  # amplitude of noise
             "same_noise_per_sim": False,  # same noise for all simulations
-
-            "sti_apply": False,          # apply stimulation
-            "iapp": 0.0,                 # external input
-            "t_start": 0.0,            # initial time    [ms]
-            "t_cut": 30_000,      # transition time [ms]
-            "t_end": 300_000,            # end time        [ms]
-
-            "num_nodes": None,           # number of nodes
-            "weights": None,             # weighted connection matrix
-            "record_step": 10,           # sampling step from r and v
-            "output": "output",          # output directory
-            "RECORD_TS": 0,              # store r and v time series
+            "sti_apply": False,  # apply stimulation
+            "iapp": 0.0,  # external input
+            "t_start": 0.0,  # initial time    [ms]
+            "t_cut": 0,  # transition time [ms]
+            "t_end": 300_000,  # end time        [ms]
+            "num_nodes": None,  # number of nodes
+            "weights": None,  # weighted connection matrix
+            "rv_decimate": 10,  # sampling step from r and v
+            "output": "output",  # output directory
+            "RECORD_RV": False,  # store r and v time series
+            "RECORD_BOLD": True,  # store BOLD signal
+            "RECORD_AVG_r": False, # store average_r 
             "num_sim": 1,
             "method": "heun",
             "engine": "cpu",
@@ -144,8 +146,8 @@ class MPR_sde:
         noise_amp = par["noise_amp"]
         sigma_r = np.sqrt(dt) * np.sqrt(2 * noise_amp)
         sigma_v = np.sqrt(dt) * np.sqrt(4 * noise_amp)
-        par['sigma_r'] = sigma_r
-        par['sigma_v'] = sigma_v
+        par["sigma_r"] = sigma_r
+        par["sigma_v"] = sigma_v
         par.update(self.get_balloon_parameters())
 
         return par
@@ -158,7 +160,7 @@ class MPR_sde:
     def prepare_input(self):
 
         self.G = self.xp.array(self.G)
-        assert (self.weights is not None), "weights must be provided"
+        assert self.weights is not None, "weights must be provided"
         self.weights = self.xp.array(self.weights).T  # ! Directed network #!TODO: check
         self.weights = move_data(self.weights, self.engine)
         self.nn = self.num_nodes = self.weights.shape[0]
@@ -170,11 +172,13 @@ class MPR_sde:
         self.t_start = self.t_start / 10.0
         self.t_cut = self.t_cut / 10.0
         self.eta = prepare_vec(self.eta, self.num_sim, self.engine, self.dtype)
+        self.J = prepare_vec(self.J, self.num_sim, self.engine, self.dtype)
+        self.delta = prepare_vec(self.delta, self.num_sim, self.engine, self.dtype)
 
     def f_mpr(self, x, t):
-        '''
+        """
         MPR model
-        '''
+        """
 
         G = self.G
         J = self.J
@@ -189,88 +193,21 @@ class MPR_sde:
         rtau = 1.0 / tau
         x0 = x[:nn, :]
         x1 = x[nn:, :]
-        dxdt = xp.zeros((2*nn, ns)).astype(self.dtype)
-        delta_over_tau_pi = delta / (tau * np.pi)
-        J_tau = J * tau
+        dxdt = xp.zeros((2 * nn, ns)).astype(self.dtype)
+        tau_pi_inv = 1.0 / (tau * np.pi)
         pi2 = np.pi * np.pi
         tau2 = tau * tau
 
         coupling = weights @ x0
-        dxdt[:nn, :] = rtau * (delta_over_tau_pi + 2 * x0 * x1)
-        dxdt[nn:, :] = rtau * (x1 * x1 + eta + iapp + J_tau *
-                               x0 - (pi2 * tau2 * x0 * x0) +
-                               G * coupling)
+        dxdt[:nn, :] = rtau * (delta * tau_pi_inv + 2 * x0 * x1)
+        dxdt[nn:, :] = rtau * (
+            x1 * x1 + eta + iapp + J * tau * x0 - (pi2 * tau2 * x0 * x0) + G * coupling
+        )
 
         return dxdt
-
-    def f_fmri(self, xin, x, t):
-
-        E0 = self.E0
-        xp = self.xp
-        nn = self.num_nodes
-        ns = self.num_sim
-        inv_tauf = self.inv_tauf
-        inv_tauo = self.inv_tauo
-        inv_taus = self.inv_taus
-        inv_alpha = self.inv_alpha
-
-        dxdt = xp.zeros((4*nn, ns)).astype(self.dtype)
-        s = x[:nn, :]
-        f = x[nn:2*nn, :]
-        v = x[2*nn:3*nn, :]
-        q = x[3*nn:, :]
-        x0in = xin[:nn, :]  # use r
-
-        dxdt[:nn, :] = x0in - inv_taus * s - inv_tauf * (f - 1.0)
-        dxdt[nn:(2*nn), :] = s
-        dxdt[(2*nn):(3*nn), :] = inv_tauo * (f - v ** (inv_alpha))
-        dxdt[3*nn:, :] = (inv_tauo) * ((f * (1.0 - (1.0 - E0) ** (1.0 / f)) / E0) -
-                                       (v ** (inv_alpha)) * (q / v))
-
-        return dxdt
-
-    def integrate_fmri(self, yin, y, t):
-        '''
-        Integrate Balloon model
-
-        Parameters
-        ----------
-        yin : array [2*nn, ns]
-            r and v time series, r is used as input
-        y : array [4*nn, ns]
-            state, update in place
-        t : float
-            time
-
-        Returns
-        -------
-        yb : array [nn, ns]
-            BOLD signal
-
-        '''
-
-        V0 = self.V0
-        K1 = self.K1
-        K2 = self.K2
-        K3 = self.K3
-
-        nn = self.num_nodes
-        self.heunDeterministic(yin, y, t)
-        yb = V0 * (K1 * (1.0 - y[(3*nn):, :]) + K2 * (1.0 - y[(3*nn):,
-                   :] / y[(2*nn):(3*nn), :]) + K3 * (1.0 - y[(2*nn):(3*nn), :]))
-        return yb
-
-    def heunDeterministic(self, yin, y, t):
-        '''Heun scheme for bold model'''
-
-        dt = self.dt_bold
-        k1 = self.f_fmri(yin, y, t)
-        tmp = y + dt * k1
-        k2 = self.f_fmri(yin, tmp, t + dt)
-        y += 0.5 * dt * (k1 + k2)
 
     def heunStochastic(self, y, t, dt):
-        '''Heun scheme to integrate MPR model with noise.'''
+        """Heun scheme to integrate MPR model with noise."""
 
         xp = self.xp
         nn = self.num_nodes
@@ -288,66 +225,39 @@ class MPR_sde:
         tmp[:nn, :] += dW_r
         tmp[nn:, :] += dW_v
 
-        k2 = self.f_mpr(tmp, t+dt)
+        k2 = self.f_mpr(tmp, t + dt)
         y += 0.5 * dt * (k1 + k2)
         y[:nn, :] += dW_r
         y[:nn, :] = (y[:nn, :] > 0) * y[:nn, :]  # set zero if negative
         y[nn:, :] += dW_v
+        
+    @staticmethod
+    def do_bold_step(r_in, s, f, ftilde, vtilde, qtilde, v, q, dtt, P):
+    
+        kappa, gamma, alpha, tau, Eo = P
+        ialpha = 1 / alpha
+        
+        s[1] = s[0] + dtt * (r_in - kappa * s[0] - gamma * (f[0] - 1))
+        f[0] = np.clip(f[0], 1, None)
+        ftilde[1] = ftilde[0] + dtt * (s[0] / f[0])
+        fv = v[0] ** ialpha  # outflow
+        vtilde[1] = vtilde[0] + dtt * ((f[0] - fv) / (tau * v[0]))
+        q[0] = np.clip(q[0], 0.01, None)
+        ff = (1 - (1 - Eo) ** (1 / f[0])) / Eo  # oxygen extraction
+        qtilde[1] = qtilde[0] + dtt * ((f[0] * ff - fv * q[0] / v[0]) / (tau * q[0]))
 
-    def eulerStochastic(self, y, t, dt):
-        '''Euler scheme'''
+        f[1] = np.exp(ftilde[1])
+        v[1] = np.exp(vtilde[1])
+        q[1] = np.exp(qtilde[1])
 
-        xp = self.xp
-        nn = self.num_nodes
-        ns = self.num_sim
+        f[0] = f[1]
+        s[0] = s[1]
+        ftilde[0] = ftilde[1]
+        vtilde[0] = vtilde[1]
+        qtilde[0] = qtilde[1]
+        v[0] = v[1]
+        q[0] = q[1]
 
-        if not self.same_noise_per_sim:
-            dW_r = self.sigma_r * xp.random.randn(nn, ns)
-            dW_v = self.sigma_v * xp.random.randn(nn, ns)
-        else:
-            dW_r = self.sigma_r * xp.random.randn(nn, 1)
-            dW_v = self.sigma_v * xp.random.randn(nn, 1)
-
-        k1 = self.f_mpr(y, t)
-        y += y + dt * k1
-        y[:nn, :] += dW_r
-        y[:nn, :] = (y[:nn, :] > 0) * y[:nn, :]  # set zero if negative
-        y[nn:, :] += dW_v
-
-    def bgStochastic(self, y, t, dt):
-        '''Bogacki Shampine scheme'''
-
-        xp = self.xp
-        nn = self.num_nodes
-        ns = self.num_sim
-
-        if not self.same_noise_per_sim:
-            dW_r = self.sigma_r * xp.random.randn(nn, ns)
-            dW_v = self.sigma_v * xp.random.randn(nn, ns)
-        else:
-            dW_r = self.sigma_r * xp.random.randn(nn, 1)
-            dW_v = self.sigma_v * xp.random.randn(nn, 1)
-
-        f = self.f_mpr
-
-        k1 = f(y, t)
-        k2 = f(y + 0.5 * dt * k1, t + 0.5 * dt)
-        k3 = f(y + 0.75 * dt * k2, t + 0.75 * dt)
-        y += dt / 9.0 * (2.0 * k1 + 3.0 * k2 + 4.0 * k3)
-
-        y[:nn, :] += dW_r
-        y[:nn, :] = (y[:nn] > 0) * y[:nn]  # set zero if negative
-        y[nn:, :] += dW_v
-
-    def set_integrator(self):
-        if self.method == "euler":
-            return self.eulerStochastic
-        elif self.method == "heun":
-            return self.heunStochastic
-        elif self.method == "bg":
-            return self.bgStochastic
-        else:
-            raise ValueError("Invalid method: " + self.method)
 
     def sync_(self, engine="gpu"):
         if engine == "gpu":
@@ -358,72 +268,92 @@ class MPR_sde:
     def run(self, verbose=True):
 
         self.prepare_input()
-        integrator = self.set_integrator()
         dt = self.dt
+        rv_decimate = self.rv_decimate
+        r_period = dt * rv_decimate
+        dtt = r_period / 1000.0  # in seconds
+        tr = self.tr
         xp = self.xp
         ns = self.num_sim
         nn = self.num_nodes
-        dec = self.decimate
         engine = self.engine
-        rs = self.record_step
-        t_cut = self.t_cut
+
         n_steps = np.ceil(self.t_end / dt).astype(int)
-        i_cut = np.ceil(t_cut / dt).astype(int)
+        bold_decimate = int(np.round(tr / r_period))
+        
+        vo = self.vo
+        k1 = 4.3 * self.theta0 * self.Eo * self.TE
+        k2 = self.epsilon * self.r0 * self.Eo * self.TE
+        k3 = 1 - self.epsilon
 
-        y0_fmri = xp.zeros((4 * nn, ns)).astype(self.dtype)
-        y0_fmri[nn:, :] = 1.0
-        y0 = copy(self.initial_state)
+        s = xp.zeros((2, nn, ns), dtype=self.dtype)
+        f = xp.zeros((2, nn, ns), dtype=self.dtype)
+        ftilde = xp.zeros((2, nn, ns), dtype=self.dtype)
+        vtilde = xp.zeros((2, nn, ns), dtype=self.dtype)
+        qtilde = xp.zeros((2, nn, ns), dtype=self.dtype)
+        v = xp.zeros((2, nn, ns), dtype=self.dtype)
+        q = xp.zeros((2, nn, ns), dtype=self.dtype)
+        vv = np.zeros((n_steps // bold_decimate + 1, nn, ns), dtype="f")
+        qq = np.zeros((n_steps // bold_decimate + 1, nn, ns), dtype="f")
+        s[0] = 1
+        f[0] = 1
+        v[0] = 1
+        q[0] = 1
+        ftilde[0] = 0
+        vtilde[0] = 0
+        qtilde[0] = 0
+        rv_curr = copy(self.initial_state)
+        rv_d = np.array([])
+        rv_t = np.array([])
+        avg_r = np.array([])
 
-        rv_d = np.array([]).astype("f")
-        fmri_t = []
-        rv_t = []
+        if self.RECORD_RV:
+            rv_d = np.zeros((n_steps // rv_decimate, 2 * nn, ns), dtype="f")
+            rv_t = np.zeros((n_steps // rv_decimate), dtype="f")
+            
+        if self.RECORD_AVG_r:
+            avg_r = np.zeros((nn, ns), dtype="f")
+            
+        cc = 0
+        for i in tqdm.trange(n_steps, disable=not verbose, desc="Integrating"):
 
-        if self.RECORD_TS:
-            _nt = np.ceil((n_steps - i_cut)/rs).astype(int)
-            rv_d = np.zeros((_nt, 2*nn, ns), dtype="f")
+            t_curr = i * dt
+            self.heunStochastic(rv_curr, t_curr, dt)
+            self.do_bold_step(rv_curr[:nn, :], s, f, ftilde, vtilde, qtilde, v, q, dtt,
+                         [self.kappa, self.gamma, self.alpha, self.tau, self.Eo])
+            # self.sync_(engine)
 
-        _nt = np.ceil((n_steps)/(rs*dec)).astype(int)
-        fmri_d = np.zeros((_nt, nn, ns), dtype="f")
+            if (i % rv_decimate) == 0:
 
-        ii = 0  # index for rv_d
-        jj = 0  # index for fmri_D
+                if self.RECORD_RV:
+                    rv_d[i // rv_decimate] = get_(rv_curr, engine, "f")
+                    rv_t[i // rv_decimate] = t_curr
+                    
+                if self.RECORD_AVG_r and i > n_steps // 2 :
+                    avg_r += get_(rv_curr[:nn, :], engine, "f")
+                    cc +=1
 
-        for it in tqdm.trange(n_steps, disable=not verbose, desc="Integrating"):
+            if i % bold_decimate == 0:
+                vv[i // bold_decimate] = get_(v[1], engine, "f") #v[1]
+                qq[i // bold_decimate] = get_(q[1], engine, "f") # q[1]
 
-            t = it * dt
-            integrator(y0, t, dt)
-            self.sync_(engine)  # !TODO: check if this is necessary
-
-            if (it % rs) == 0:  # and (it >= i_cut):
-
-                if self.RECORD_TS and (it >= i_cut):
-                    rv_d[ii, :, :] = get_(y0, engine, "f")
-                    rv_t.append(t)
-                    ii += 1
-
-                fmri_i = self.integrate_fmri(y0, y0_fmri, t)
-                self.sync_(engine)
-
-            if it % (dec*rs) == 0:
-                fmri_d[jj, :, :] = get_(fmri_i, engine, "f")
-                jj += 1
-                fmri_t.append(t)
-        fmri_d = np.asarray(fmri_d).astype("f")
-        fmri_t = np.asarray(fmri_t).astype("f")
-        fmri_d = fmri_d[fmri_t >= t_cut, :, :]
-        fmri_t = fmri_t[fmri_t >= t_cut] * 10.0
+        bold_d = vo * (k1 * (1 - qq) + k2 * (1 - qq / vv) + k3 * (1 - vv))
+        bold_t = np.linspace(0, self.t_end - dt * bold_decimate, len(bold_d))
+        bold_t = bold_t * 10.0
         rv_t = np.asarray(rv_t).astype("f") * 10.0
+        avg_r = avg_r / cc
 
         return {
             "rv_t": rv_t,
             "rv_d": rv_d,
-            "fmri_d": fmri_d,
-            "fmri_t": fmri_t,
+            "fmri_t": bold_t,
+            "fmri_d": bold_d,
+            "avg_r": avg_r,
         }
 
 
 def set_initial_state(nn, ns, engine, seed=None, same_initial_state=False, dtype=float):
-    '''
+    """
     Set initial state
 
     Parameters
@@ -441,19 +371,21 @@ def set_initial_state(nn, ns, engine, seed=None, same_initial_state=False, dtype
     dtype : str
         float: float64
         f    : float32
-    '''
+    """
 
     if seed is not None:
         np.random.seed(seed)
 
     if same_initial_state:
-        y0 = np.random.rand(2*nn)
+        y0 = np.random.rand(2 * nn)
         y0 = repmat_vec(y0, ns, engine)
     else:
-        y0 = np.random.rand(2*nn, ns)
+        y0 = np.random.rand(2 * nn, ns)
         y0 = move_data(y0, engine)
 
     y0[:nn, :] = y0[:nn, :] * 1.5
     y0[nn:, :] = y0[nn:, :] * 4 - 2
 
     return y0.astype(dtype)
+
+
