c3bc8c69873307edf3c93ca6c93847aa8ad0b5d9
Abolfazl  Ziaeemehr <a.ziaeemehr@gmail.com>
2025-02-03 15:33:26 +0100

    Refactor Cupy utils module to handle data movement between CPU and GPU

--- vbi/models/cupy/utils.py
+++ vbi/models/cupy/utils.py
@@ -4,7 +4,7 @@ from numpy.matlib import repmat
 try:
     import cupy as cp
 except:
-    pass
+    cp = None
 
 
 def get_module(engine="gpu"):
@@ -20,7 +20,7 @@ def get_module(engine="gpu"):
 
 def tohost(x):
     '''
-    move data to cpu
+    move data to cpu if it is on gpu
 
     Parameters
     ----------
@@ -32,7 +32,9 @@ def tohost(x):
     array
         data moved to cpu
     '''
-    return cp.asnumpy(x)
+    if cp is not None and isinstance(x, cp.ndarray):
+        return cp.asnumpy(x)
+    return x
 
 
 def todevice(x):
