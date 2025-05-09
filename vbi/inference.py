3355c7eb0249963f7777b0dc21d2b9fbe2d34293
Abolfazl  Ziaeemehr <a.ziaeemehr@gmail.com>
2025-05-09 15:04:24 +0200

    refactor: Improve code formatting and style in Inference class methods

--- vbi/inference.py
+++ vbi/inference.py
@@ -3,37 +3,42 @@ from vbi.utils import *
 from sbi.inference import SNPE, SNLE, SNRE
 from sbi.utils.user_input_checks import process_prior
 
+
 class Inference(object):
     def __init__(self) -> None:
         pass
 
     @timer
-    def train(self,
-              theta,
-              x,
-              prior,
-              num_threads=1,
-              method="SNPE",
-              device="cpu",
-              density_estimator="maf"
-              ):
+    def train(
+        self,
+        theta,
+        x,
+        prior,
+        num_threads=1,
+        method="SNPE",
+        device="cpu",
+        density_estimator="maf",
+    ):
 
         torch.set_num_threads(num_threads)
 
-        if (len(x.shape) == 1):
+        if len(x.shape) == 1:
             x = x[:, None]
-        if (len(theta.shape) == 1):
+        if len(theta.shape) == 1:
             theta = theta[:, None]
 
         if method == "SNPE":
             inference = SNPE(
-                prior=prior, density_estimator=density_estimator, device=device)
+                prior=prior, density_estimator=density_estimator, device=device
+            )
         elif method == "SNLE":
             inference = SNLE(
-                prior=prior, density_estimator=density_estimator, device=device)
+                prior=prior, density_estimator=density_estimator, device=device
+            )
         elif method == "SNRE":
             inference = SNRE(
-                prior=prior, density_estimator=density_estimator, device=device)
+                prior=prior, density_estimator=density_estimator, device=device
+            )
         else:
             raise ValueError("Invalid method: " + method)
 
@@ -45,7 +50,7 @@ class Inference(object):
 
     @staticmethod
     def sample_prior(prior, n, seed=None):
-        '''
+        """
         sample from prior distribution
 
         Parameters
@@ -58,19 +63,17 @@ class Inference(object):
         Returns
         -------
 
-        '''
+        """
         if seed is not None:
             torch.manual_seed(seed)
-            
+
         prior, _, _ = process_prior(prior)
         theta = prior.sample((n,))
         return theta
 
     @staticmethod
-    def sample_posterior(xo,
-                         num_samples,
-                         posterior):
-        '''
+    def sample_posterior(xo, num_samples, posterior):
+        """
         sample from the posterior using the given observation point.
 
         Parameters
@@ -87,7 +90,7 @@ class Inference(object):
         samples: torch.tensor float32 (num_samples, d)
             samples from the posterior
 
-        '''
+        """
 
         if not isinstance(xo, torch.Tensor):
             xo = torch.tensor(xo, dtype=torch.float32)
